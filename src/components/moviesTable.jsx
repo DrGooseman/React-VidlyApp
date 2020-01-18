c520948c3452b3f69cffd8e5a66d70590aa18531
DrGooseman <jamesjquinn12@gmail.com>
2020-01-18 22:16:56 +0300

    extracted the table logic from moviesTable into its own component

--- src/components/moviesTable.jsx
+++ src/components/moviesTable.jsx
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import Like from "./common/like";
-import TableHeader from "./common/tableHeader";
-import TableBody from "./common/tableBody";
+import Table from "./common/table";
 
 class MoviesTable extends Component {
   columns = [
@@ -35,14 +34,12 @@ class MoviesTable extends Component {
     const { movies, onDelete, sortColumn, onSort, onLike } = this.props;
 
     return (
-      <table className="table">
-        <TableHeader
-          columns={this.columns}
-          sortColumn={sortColumn}
-          onSort={onSort}
-        ></TableHeader>
-        <TableBody columns={this.columns} data={movies} />
-      </table>
+      <Table
+        columns={this.columns}
+        data={movies}
+        sortColumn={sortColumn}
+        onSort={onSort}
+      />
     );
   }
 }
