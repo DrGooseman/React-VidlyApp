34789928bd0933ea563ea0b27bf799ea2ff0e2ef
DrGooseman <jamesjquinn12@gmail.com>
2020-01-22 16:51:55 +0300

    added logout + refactored auth logic into single component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import { Route, Redirect, Switch } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
-import jwtDecode from "jwt-decode";
 import Movies from "./components/movies";
 import Rentals from "./components/rentals";
 import NotFound from "./components/notFound";
@@ -10,6 +9,8 @@ import NavBar from "./components/navBar";
 import MovieForm from "./components/movieForm";
 import LoginForm from "./components/loginForm";
 import RegisterForm from "./components/registerForm";
+import Logout from "./components/logout";
+import auth from "./services/authService";
 import "./App.css";
 import "react-toastify/dist/ReactToastify.css";
 
@@ -17,12 +18,10 @@ class App extends Component {
   state = {};
 
   componentDidMount() {
-    try {
-      const jwt = localStorage.getItem("token");
-      const user = jwtDecode(jwt);
-      this.setState({ user });
-    } catch (ex) {}
+    const user = auth.getCurrentUser();
+    this.setState({ user });
   }
+
   render() {
     return (
       <React.Fragment>
@@ -32,6 +31,7 @@ class App extends Component {
           <Switch>
             <Route path="/register" component={RegisterForm}></Route>
             <Route path="/login" component={LoginForm}></Route>
+            <Route path="/logout" component={Logout}></Route>
             <Route path="/movies/:id" component={MovieForm}></Route>
             <Route path="/movies/new" component={Customers}></Route>
             <Route path="/movies" component={Movies}></Route>
